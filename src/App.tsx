e62d77707df8374786f4d52113db6708030ff953
liuyang <flow_ocean@yahoo.com>
2023-01-06 14:26:16 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Button, Card, Form, Input} from "antd";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [form] = Form.useForm()
+    const [checkNick, setCheckNick] = useState(false)
+
+    useEffect(() => {
+        form.validateFields(['nickname'])
+    }, [checkNick, form])
+
+    const onCheck = async () => {
+        try {
+            const values = await form.validateFields()
+            console.log('sucess', values)
+        } catch (erroInfo) {
+            console.log('Faild, ', erroInfo)
+        }
+    }
+    return (
+        <div>
+            <div style={{textAlign: 'center'}}>
+                <img src='/logo.png' width='50%'/>
+            </div>
+
+            <Card style={{margin: 20}}>
+                <Form form={form}>
+                    <Form.Item name='username' label='Name' rules={[
+                        {
+                            required: true,
+                            message: 'Please input your name',
+                        },
+                    ]}>
+                        <Input placeholder='Please input your name'/>
+                    </Form.Item>
+                    <Form.Item label='NO KP' name='nokp' rules={[
+                        {
+                            required: true,
+                            message: 'Please input NO KP'
+                        }
+                    ]}>
+                        <Input placeholder='Please input NO KP'/>
+                    </Form.Item>
+                    <Form.Item label='Document Control Number' name='docnumber' rules={[
+                        {
+                            required: true,
+                            message: 'Please input document control number'
+                        }
+                    ]}>
+                        <Input placeholder='Please input document control number'/>
+                    </Form.Item>
+                </Form>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <Button type='primary' onClick={onCheck}>Digital Signature</Button>
+                </div>
+                <div style={{marginTop: 20}}>
+                    I acknowledge that this authorization will remain in effect bla bla bla...
+                </div>
+            </Card>
+        </div>
+    );
 }
 
 export default App;
